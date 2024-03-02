3bb86b6364c84610df9510689e7e8f236c24b42c
kaka <luukaka2211@gmail.com>
2024-03-02 13:12:21 +0700

    Up chức năng view admin, update view booking agency

--- FrontEnd/real_estate_v3/src/App.js
+++ FrontEnd/real_estate_v3/src/App.js
@@ -1,3 +1,8 @@
+// React imports
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+// CSS imports
 import './css/header.css';
 import './css/footer.css';
 import './css/login.css';
@@ -15,25 +20,28 @@ import './css/investor-chinhsuatindang.css';
 import './css/agency-duyettindang.css';
 import './css/agency-xemlichdat.css';
 import './css/customer-dondat.css';
-import './css/investor-thongtinchitiet.css'
-import './css/customer-thongtinchitiet.css'
-import './css/investor-dangtinmain.css'
-import './css/investor-naptien.css'
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import './css/investor-thongtinchitiet.css';
+import './css/customer-thongtinchitiet.css';
+import './css/investor-dangtinmain.css';
+import './css/investor-naptien.css';
+
+// Component imports
 import Header from './header-footer/Header';
 import Header2 from './header-footer/Header2';
 import Footer from './header-footer/Footer';
-import Body1 from './components/Body1';
 import Login from './components/Login';
 import Dangki from './components/Dangki';
 import Logout from './components/Logout';
-import Home2 from './components/Home2';
 
-import Adminmain from './components/Admin/admin-main';
-import Adminallaccount from './components/Admin/admin-allaccount';
+// Customer Components
+import TrangChu from './components/Customer/customer-trangchu';
+import Customergioithieu from './components/Customer/customer-gioithieu';
+import Customerdondat from './components/Customer/customer-dondat';
+import Customerlienhe from './components/Customer/customer-lienhe';
+import Customerthongtintaikhoan from './components/Customer/customer-thongtintaikhoan';
+import Customerthongtinchitiet from './components/Customer/customer-thongtinchitiet';
 
-
+// Investor Components
 import InvestorDangtin from './components/Investor/investor-dangtin';
 import Quanlitindang from './components/Investor/investor-quanlitindang';
 import Investorthongtintaikhoan from './components/Investor/investor-thongtintaikhoan';
@@ -42,92 +50,63 @@ import Investorthongtinchitiet from './components/Investor/investor-thongtinchit
 import Investordangtinmain from './components/Investor/investor-dangtinmain';
 import InvestorNaptien from './components/Investor/investor-naptien';
 
-import AuthRoleFilter from './authentication/AuthRoleFilter';
+// Agency Components
 import Agencytindang from './components/Agency/agency-tindang';
 import Agencythongtinchitiet from './components/Agency/agency-thongtinchitiet';
 import Agencyduyettindang from './components/Agency/agency-duyettindang';
 import Agencyxemlichdat from './components/Agency/agency-xemlichdat';
 
+// Admin Components
+import Adminmain from './components/Admin/admin-main';
+import AdminDetailBookingAgen from './components/Admin/admin-DetailBookingAgen';
 
-import TrangChu from './components/Customer/customer-trangchu';
-import Customergioithieu from './components/Customer/customer-gioithieu';
-import Customerthongtinchitiet from './components/Customer/customer-thongtinchitiet';
-import Customerthongtintaikhoan from './components/Customer/customer-thongtintaikhoan';
-import Customerlienhe from './components/Customer/customer-lienhe';
-import Customerdondat from './components/Customer/customer-dondat';
 function App() {
   const [userInfo, setUserInfo] = useState(null);
   const [initialPageLoad, setInitialPageLoad] = useState(true);
 
-  const handleLoginSuccess = (userLoginBasicInformationDto) => {
-    // Callback function to update user information in App.js state
-    setUserInfo(userLoginBasicInformationDto);
-  };
-
   useEffect(() => {
-    // Once the component mounts, update the state to indicate that the initial page load is complete
     setInitialPageLoad(false);
   }, []);
 
   return (
     <Router>
       <div className="App">
-        {/* Header should be rendered outside Routes */}
         <Header />
         <Header2 />
         <Routes>
-          <Route
-            path="/"
-            element={initialPageLoad ? <Navigate to="/trangchu" replace /> : <Navigate to="/" />}
-          />
-          <Route path="/dangnhap" element={<Login onLoginSuccess={handleLoginSuccess} />} />
-          {/* <Route
-            path="/home2"
-            element={
-              <AuthRoleFilter>
-                <Home2 />
-              </AuthRoleFilter>
-            }
-          /> */}
-          {/* <Route
-            path="/home"
-            element={
-              <AuthRoleFilter>
-                <Home />
-              </AuthRoleFilter>
-            }
-          /> */}
+          {/* Public Routes */}
+          <Route path="/" element={initialPageLoad ? <Navigate to="/trangchu" replace /> : <TrangChu />} />
+          <Route path="/dangnhap" element={<Login />} />
+          <Route path="/dangki" element={<Dangki />} />
+          <Route path="/logout" element={<Logout />} />
 
-          <Route path="/Logout" element={<Logout />} /> done
-          <Route path="/dangki" element={<Dangki />} /> done
-          <Route path="/trangchu" element={<TrangChu />} /> done
-
-          <Route path="/adminmain" element={<Adminmain />} /> done
-
-          <Route path="/quanlitindang" element={<Quanlitindang />} />  done
-          <Route path="/acencytindang" element={<Agencytindang />} />done
-          <Route path="/agencythongtinchitiet" element={<Agencythongtinchitiet />} /> 
-          <Route path="/agencythongtinchitiet/:id" element={<Agencythongtinchitiet />} />
-          <Route path="/xemlichdat" element={<Agencyxemlichdat />} /> done
-          <Route path="/duyettindang" element={<Agencyduyettindang />} /> done
-
-          <Route path="/inestortindangmain" element={<Investordangtinmain />} />done
-          <Route path="/chinhsuatindang" element={<InvestorTindang />} /> done
-          <Route path="/investorthongtintaikhoan" element={<Investorthongtintaikhoan />} /> done
-          <Route path="/investorthongtinchitiet" element={<Investorthongtinchitiet />} />done
-          <Route path="/investorthongtinchitiet/:id" element={<Investorthongtinchitiet />} />done
-          <Route path="/investordangtin" element={<InvestorDangtin />} /> done
-          <Route path="/investornaptien" element={<InvestorNaptien />} />done
-
-          <Route path="/gioithieu" element={<Customergioithieu />} /> done
-          <Route path="/dondat" element={<Customerdondat />} /> done
-          <Route path="/lienhe" element={<Customerlienhe />} /> done
-          <Route path="/customerthongtintaikhoan" element={<Customerthongtintaikhoan />} />done
-          <Route path="/thongtinchitietbatdongsan" element={<Customerthongtinchitiet />} />
+          {/* Customer Routes */}
+          <Route path="/trangchu" element={<TrangChu />} />
+          <Route path="/gioithieu" element={<Customergioithieu />} />
+          <Route path="/dondat" element={<Customerdondat />} />
+          <Route path="/lienhe" element={<Customerlienhe />} />
+          <Route path="/customerthongtintaikhoan" element={<Customerthongtintaikhoan />} />
           <Route path="/thongtinchitietbatdongsan/:id" element={<Customerthongtinchitiet />} />
-          {/* Remove the default route */}
+
+          {/* Investor Routes */}
+          <Route path="/investordangtin" element={<InvestorDangtin />} />
+          <Route path="/quanlitindang" element={<Quanlitindang />} />
+          <Route path="/investorthongtintaikhoan" element={<Investorthongtintaikhoan />} />
+          <Route path="/investorthongtinchitiet/:id" element={<Investorthongtinchitiet />} />
+          <Route path="/chinhsuatindang" element={<InvestorTindang />} />
+          <Route path="/investordangtinmain" element={<Investordangtinmain />} />
+          <Route path="/investornaptien" element={<InvestorNaptien />} />
+
+          {/* Agency Routes */}
+          <Route path="/agencytindang" element={<Agencytindang />} />
+          <Route path="/agencythongtinchitiet/:id" element={<Agencythongtinchitiet />} />
+          <Route path="/xemlichdat" element={<Agencyxemlichdat />} />
+          <Route path="/duyettindang" element={<Agencyduyettindang />} />
+
+          {/* Admin Routes */}
+          <Route path="/adminmain" element={<Adminmain />} />
+          <Route path="/admin-DetailBookingAgen/:id" element={<AdminDetailBookingAgen />} />
         </Routes>
-        {/* Footer should be rendered outside Routes */}
         <Footer />
       </div>
     </Router>
--- FrontEnd/real_estate_v3/src/components/Admin/admin-main.js
+++ FrontEnd/real_estate_v3/src/components/Admin/admin-main.js
@@ -2,11 +2,16 @@ import React from 'react'
 import Adminallaccount from './admin-allaccount'
 import AdminCreaccountagency from './admin-creaccountagency'
 import AdminAllReservation from './admin-AllReservation'
+import AdminAgencyBooking from './admin-AgencyBooking'
+import AdminViewCompleteBooking from './admin-ViewCompleteBooking'
 export default function Adminmain() {
     return (
         <div>
+            
             {/* <Adminallaccount /> */}
+            <AdminAgencyBooking />
             <AdminAllReservation />
+            <AdminViewCompleteBooking />
             {/* <AdminCreaccountagency /> */}
         </div>
     )
