3c03bd6a600fa78e4b373b172461f75a0e7a50b3
DatVX02 <143780039+DatVX02@users.noreply.github.com>
2024-03-06 10:37:46 +0700

    update v2

--- FrontEnd/real_estate_v3/src/list/dichvufooter.js
+++
@@ -1,9 +0,0 @@
-// dichvufooter.js
-const dichvufooter = [
-    { id: 1, name: 'Dịch vụ 1', link: '/dichvu-1' },
-    { id: 2, name: 'Dịch vụ 2', link: '/dichvu-2' },
-    { id: 3, name: 'Dịch vụ 3', link: '/dichvu-3' },
-    // Add more services as needed
-];
-
-export default dichvufooter;
